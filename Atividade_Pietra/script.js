25e709ecbc9181caae0197ae4f26961f0f5c9d91
Matheus targa <matheus.targa@gmail.com>
2025-09-11 21:43:06 -0300

    first commit

---
+++ Atividade_Pietra/script.js
@@ -0,0 +1,52 @@
+const meuBotao = document.getElementById('bt_entrar');
+
+const campoEmaill = document.getElementById('inputEmail3');
+
+const campoSenha = document.getElementById('inputPassword3');
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const emailCorreto = "teste@empresa.com" ;
+const senhaCorreta = "123456";
+
+
+meuBotao.addEventListener("click",function (event) {
+    event.preventDefault();
+
+    const email = campoEmaill.value;
+    const senha = campoSenha.value;
+    
+
+    if (email.length == "") {
+        alert("Atenção o campo de E-mail deve ser Preenchido")
+
+    }else if(senha.length == ""){
+        alert("Atenção A senha deve ser preenchida")
+    }
+
+    if(email.length != ""){
+
+            if (regexEmail.test(email)) {
+                console.log("email ok")
+                    if (senha.length < 6) {
+                        alert("Senha Inválida")
+                    }else{
+                        if (email === emailCorreto) {
+                            if (senha === senhaCorreta) {
+                                alert("Login Feito com Sucesso")
+                            }else{
+                                alert("Senha Incorreta")
+                            }
+                        }else{
+                            alert("Email Incorreto")
+                        }
+                    }
+            }else{
+                alert("E-mail Inválido")
+            }
+
+    }
+
+})
+
+
+
